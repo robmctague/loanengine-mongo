a103cf4b1896244cd5dc1bbff73c6920808834c1
Rob McTague <robert.mctague@gmail.com>
2019-11-04 20:53:07 +0000

    initial commit

---
+++ customers/customers/Models/address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace customers.Models
+{
+    public class address
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string AddressLine1 { get; set; }
+
+        public string AddressLine2 { get; set; }
+
+        public string AddressLine3 { get; set; }
+
+        public string AddressLine4 { get; set; }
+
+        public string AddressLine5 { get; set; }
+
+        public string AddressType { get; set; }
+
+        public address(string AddressLine1)
+        {
+            this.AddressLine1 = AddressLine1;
+        }
+    }
+}
---
+++ customers/customers/Models/customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace customers.Models
+{
+    public class customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DOB { get; set; }
+
+
+    }
+}
